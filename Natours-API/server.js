2435acefd0eea9eb7a79f75ab900e0911a823eaa
Leroy <ntruderleroy@gmail.com>
2025-03-26 10:24:05 +0300

    mongoose added

--- Natours-API/server.js
+++ Natours-API/server.js
@@ -1,8 +1,24 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
 
-console.log(process.env);
+const DB = process.env.DATABASE.replace(
+	"PASSWORD",
+	process.env.DATABASE_PASSWORD
+);
 
-const port = 3000;
+mongoose
+	.connect(DB)
+	.then((con) => {
+		console.log("DB connection successful");
+	})
+	.catch((err) => {
+		console.error("DB connection error:", err);
+	});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
