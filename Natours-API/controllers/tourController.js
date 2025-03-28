0206d17a40f8ec72e829c1ce0632ac05c9d4f552
Leroy <ntruderleroy@gmail.com>
2025-03-28 11:00:36 +0300

    more mongodb and mongoose tings

--- Natours-API/controllers/tourController.js
+++ Natours-API/controllers/tourController.js
@@ -1,12 +1,31 @@
 const Tour = require("../models/tourModel");
+const APIFeatures = require("../utils/apiFeatures");
 
 // const tours = JSON.parse(
 // 	fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
+exports.aliasTopTours = (req, res, next) => {
+	req.query.limit = "4";
+	req.query.sort = "-ratingsAverage,price";
+	req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+	next();
+};
+
 exports.getAllTours = async (req, res) => {
 	try {
-		const tours = await Tour.find();
+		// execute the query
+		const features = new APIFeatures(Tour.find(), req.query)
+			.filter()
+			.sort()
+			.limitFields()
+			.paginate();
+
+		const tours = await features.query;
+
+		// query.sort().select().skip().limit()
+
+		// send the response
 		res.status(200).json({
 			status: "success",
 			results: tours.length,
@@ -83,6 +102,96 @@ exports.deleteTour = async (req, res) => {
 		res.status(204).json({
 			status: "success",
 			data: null,
+		});
+	} catch (err) {
+		res.status(404).json({
+			status: "failed",
+			message: err,
+		});
+	}
+};
+
+exports.getTourStats = async (req, res) => {
+	try {
+		const stats = await Tour.aggregate([
+			{
+				$match: { ratingsAverage: { $gte: 4.5 } },
+			},
+			{
+				$group: {
+					_id: { $toUpper: "$difficulty" },
+					numTours: { $sum: 1 },
+					numRatings: { $sum: "$ratingsQuantity" },
+					avgRating: { $avg: "$ratingsAverage" },
+					avgPrice: { $avg: "$price" },
+					minPrice: { $min: "$price" },
+					maxPrice: { $max: "$price" },
+				},
+			},
+			{
+				$sort: { avgPrice: 1 },
+			},
+			// {
+			// 	$match: { _id: { $ne: "EASY" } },
+			// },
+		]);
+
+		res.status(200).json({
+			status: "success",
+			data: {
+				stats,
+			},
+		});
+	} catch (err) {
+		res.status(404).json({
+			status: "failed",
+			message: err,
+		});
+	}
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+	try {
+		const year = req.params.year * 1;
+		const plan = await Tour.aggregate([
+			{
+				$unwind: "$startDates",
+			},
+			{
+				$match: {
+					startDates: {
+						$gte: new Date(`${year}-01-01`),
+						$lte: new Date(`${year}-12-31`),
+					},
+				},
+			},
+			{
+				$group: {
+					_id: { $month: "$startDates" },
+					numTourStarts: { $sum: 1 },
+					tours: { $push: "$name" },
+				},
+			},
+			{
+				$addFields: { month: "$_id" },
+			},
+			{
+				$project: {
+					_id: 0,
+				},
+			},
+			{
+				$sort: { numberTourStats: -1 },
+			},
+			{
+				$limit: 12,
+			},
+		]);
+		res.status(200).json({
+			status: "success",
+			data: {
+				plan,
+			},
 		});
 	} catch (err) {
 		res.status(404).json({
